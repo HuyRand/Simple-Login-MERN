6c6e194894f567682ac5ab7189078ad72107277b
HuyRand <82890282+HuyRand@users.noreply.github.com>
2022-12-09 02:46:58 +0700

    initalize api fetching

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+
+
 
 function App() {
+
+  const[BackEndData, setBackEndData] = useState([{}])
+
+  useEffect(()=>{
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackEndData(data)
+      }
+    )
+  },[])
+
   return (
-    <div>App</div>
+    <div>
+      
+    </div>
   )
 }
 
